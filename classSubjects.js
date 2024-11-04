cca947b0d9f60ea7e2b4781882685a480ffaacab
John Rhay Domingo <jhnrhyyyyyyyyyy@gmail.com>
2024-11-04 18:30:16 +0800

    Class Subjects

--- classSubjects.js
+++ classSubjects.js
@@ -4,4 +4,4 @@ let classroom = "Computer Lab A";
 let classInstructor = "Vj Barayuga";
 let studentName = "John Rhay B. Domingo";
 
-console.log(""+studentName+", is currently enrolled in "+subjectTitle+", with a class schedule of "+classSchedule+", at room "+classroom+". The instructor for the subject is "+classInstructor);
+console.log(""+studentName+" is currently enrolled in "+subjectTitle+" with a class schedule of "+classSchedule+" at room "+classroom+". The instructor for the subject is "+classInstructor);
